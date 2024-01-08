ad7820f3f532dedb6474ee11e8874dc6cae498fc
nicolai.boettger <nicolai.boettger@stud.hs-hannover.de>
2024-01-08 01:12:08 +0100

    supabase connection

--- App.js
+++ App.js
@@ -2,14 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import Auth from './src/screens/Auth'
+import { Session } from '@supabase/supabase-js';
+import { supabase } from './src/lib/supabase';
+import { useEffect, useState } from 'react';
 
 
 export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  console.log(process.env.SUPABASE_ANON_KEY)
+
   return (
-    <NavigationContainer>
-      <AppNavigator>
-      </AppNavigator>
-    </NavigationContainer>
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
+    </View>
   );
 }
 
